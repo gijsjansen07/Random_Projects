ae7ce3acf045b334807ee0520e3b0a6fc7985665
Gijs Jansen <gijsjansen2007.com>
2024-08-01 22:56:46 +0200

    FInal auto clicking script for mob farm

--- #Auto Farming script Minecraft.py
+++ #Auto Farming script Minecraft.py
@@ -1,21 +1,54 @@
 #Auto clicking script Minecraft
-
-from time import sleep
+from pynput import keyboard
 from pyautogui import leftClick
-
-
-
+from time import sleep
 x = 1280
 y = 720
-time = 0
+time_not_pressed = 0 
 
+def on_press(key):
+
+    global time_not_pressed
+    time_not_pressed = 0
+
+    try:
+        print('alphanumeric key {0} pressed'.format(
+            key.char))
+    except AttributeError:
+        print('special key {0} pressed'.format(
+            key))
+
+def on_release(key):
+
+    global time_not_pressed
+    
+    print('{0} released'.format(
+        key))
+    if key == keyboard.Key.esc:
+        # Stop listener             
+        return False
+    elif time_not_pressed > 20:
+        return False
+    
+    time_not_pressed = 0
+
+# ...or, in a non-blocking fashion:
+listener = keyboard.Listener(
+    on_press=on_press,
+    on_release=on_release)
+
+
+listener.start()
 while True:
-    leftClick(x,y,3)
-    time += 3
-    print("click")
-    if time == 60 or time > 60:
-
+    sleep(1)
+    time_not_pressed += 1 
+    print(time_not_pressed)
+    if time_not_pressed > 20:
         break; 
 
-
-print (" broken")
+while time_not_pressed > 20:
+    leftClick(x,y,3)
+    time_not_pressed+=3
+    print(time_not_pressed)
+    
+print("Program stopped")
